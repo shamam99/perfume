953d64cbd36c7f96a2ba270a9a326316aa9cdc92
shamam <kafri.shamam@gmail.com>
2025-03-03 11:21:53 +0300

    Initial Commit

---
+++ perfume/perfumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  perfumeApp.swift
+//  perfume
+//
+//  Created by Shamam Alkafri on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct perfumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ perfumeTests/perfumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  perfumeTests.swift
+//  perfumeTests
+//
+//  Created by Shamam Alkafri on 03/03/2025.
+//
+
+import Testing
+@testable import perfume
+
+struct perfumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
